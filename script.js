3a8da0aae1b2afdf998232a3b6582f534be05213
lena mukhtar <lena.mukhtar@gmail.com>
2025-04-07 12:29:08 -0400

    adding part3 to js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let student = {
     enrolled: true,
     course:[" javascript ", " database ", " operatingSystem "],
     studentInfo:function() {
-      console.log("hi my name is " + this.name + "\n"+ "Enrolled ? " + this.enrolled +"\n" +" in the following classes"+this.course);
+      console.log("hi my name is " + this.name + "\n"+ "Enrolled ? " + this.enrolled +"\n" +" in the following classes"+this.course+"\n");
 
     }
 
@@ -23,6 +23,19 @@ let objectStudent = JSON.parse(jsonStudent);
 console.log(objectStudent);
 
 //part3: using Destructuring Assginment
+let [course1, course2, course3]= student.course;
+console.log("\n"+"first class: " + course1);
+console.log("second class: " + course2);
+console.log("third class: " + course3);
+
+
 
 let scores= [85,92,78,90];
+let [grade1, grade2, grade3, grade4]=scores;
+console.log("grade1= " + grade1);
+console.log("grade2= " + grade2);
+
+//part4: the spread operator
+
+
 
